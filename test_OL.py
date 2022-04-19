79de75f63c1db9a7cbdfe7b5034127213f72f1ff
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-04-19 20:42:07 +0300

    Update test_OL.py

--- test_OL.py
+++ test_OL.py
@@ -9,13 +9,14 @@ class MyTests(unittest.TestCase):
     def test_del(self):
         n = OrderedList(False)
         n.add(50)
+        n.add(51)
         n.delete(50)
-        self.assertEqual(n.len(),0)
+        self.assertEqual(n.len(),1)
         for i in range(100):
             n.add(random.randint(0,100))
         for i in range(100):
             n.delete(random.randint(0,100))
-        self.assertEqual(n.len(),0)
+        self.assertEqual(n.len(),1)
 
     def test_add(self):
         ...
