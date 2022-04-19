f5c912418ea8c34725e744a1783e3431b1018c30
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-04-19 21:33:05 +0300

    Update test_OL.py

--- test_OL.py
+++ test_OL.py
@@ -5,6 +5,19 @@ from OL import OrderedList
 from OL import Node
 import random
 class MyTests(unittest.TestCase):
+    def test_find(self):
+        n = OrderedList(False)
+        self.assertEqual(None,n.find(0))
+        for i in range(random.randint(0,1000)):
+            n.add(random.randint(0,1000))
+        for i in range(random.randint(0,1000)):
+            n.find(random.randint(0,1000))
+        n1 = OrderedList(True)
+        self.assertEqual(None,n1.find(0))
+        for i in range(random.randint(0,1000)):
+            n1.add(random.randint(0,1000))
+        for i in range(random.randint(0,1000)):
+            n1.find(random.randint(0,1000))
 
     def test_del(self):
         n = OrderedList(False)
@@ -12,14 +25,34 @@ class MyTests(unittest.TestCase):
         n.add(51)
         n.delete(50)
         self.assertEqual(n.len(),1)
+        n.delete(51)
+        self.assertEqual(n.len(),0)
         for i in range(100):
-            n.add(random.randint(0,100))
+            n.add(i)
         for i in range(100):
-            n.delete(random.randint(0,100))
-        self.assertEqual(n.len(),1)
+            n.delete(i)
+        self.assertEqual(n.len(),0)
+
+        n1 = OrderedList(False)
+        n1.add(50)
+        n1.add(51)
+        n1.delete(50)
+        self.assertEqual(n1.len(),1)
+        n1.delete(51)
+        self.assertEqual(n1.len(),0)
+        for i in range(100):
+            n1.add(i)
+        for i in range(100):
+            n1.delete(i)
+        self.assertEqual(n1.len(),0)
 
     def test_add(self):
-        ...
+        n = OrderedList(False)
+        for i in range(random.randint(0,1000)):
+            n.add(random.randint(0,1000))
+        n1 = OrderedList(False)
+        for i in range(random.randint(0,1000)):
+            n1.add(random.randint(0,1000))
 
 if __name__ == '__main__':
     unittest.main()
